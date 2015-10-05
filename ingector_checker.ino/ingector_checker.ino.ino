5b7224090ac66f1cb87923899933785ca8226087
Pavel Kartsev <modemoid@gmail.com>
2015-10-05 22:51:04 +0600

    временно добавлен другой проект

---
+++ ingector_checker.ino/ingector_checker.ino.ino
@@ -0,0 +1,40 @@
+const int ledPin =  13;      // the number of the LED pin
+const int IngectorPin =  13;      // the number of the LED pin
+
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(IngectorPin, OUTPUT);
+ 
+ // initialize the pushbutton pin as an input:
+ // pinMode(buttonPin, INPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  // read the input on analog pin 0:
+  unsigned int sensorValue0 = analogRead(A0); //work 
+    // map it to the range of the analog out:
+unsigned int   outputValue0 = sensorValue0;
+//unsigned int   outputValue0 = map(sensorValue0, 0, 1023, 0, 1023);
+  // read the input on analog pin 1:
+  int sensorValue1 = analogRead(A1);//relax
+  unsigned int outputValue1 = map(sensorValue1, 0, 1023, 0, 100);
+#define ofvmult 40
+#define serial
+#ifdef serial
+  Serial.print((outputValue0*ofvmult)/1000);
+  Serial.print(".");
+  Serial.print((outputValue0*ofvmult)%1000);
+  Serial.print("ms reax= ");
+  Serial.print(outputValue1);
+  Serial.println(" ms ");
+#endif
+digitalWrite(IngectorPin, HIGH);
+delayMicroseconds(outputValue0*ofvmult);
+digitalWrite(IngectorPin, LOW);
+delay(outputValue1);
+
+
+}
