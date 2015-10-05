501eceba8ec6e24ae5688dcda6e0ac4bc3caa36e
Pavel Kartsev <modemoid@gmail.com>
2015-10-05 23:15:21 +0600

    т

--- ingector_checker.ino/ingector_checker.ino.ino
+++ ingector_checker.ino/ingector_checker.ino.ino
@@ -16,20 +16,21 @@ void loop() {
   // read the input on analog pin 0:
   unsigned int sensorValue0 = analogRead(A0); //work 
     // map it to the range of the analog out:
-unsigned int   outputValue0 = sensorValue0;
+unsigned int   outputValue0 = sensorValue0/5;
 //unsigned int   outputValue0 = map(sensorValue0, 0, 1023, 0, 1023);
   // read the input on analog pin 1:
   int sensorValue1 = analogRead(A1);//relax
   unsigned int outputValue1 = map(sensorValue1, 0, 1023, 0, 100);
 #define ofvmult 40
-#define serial
+//#define serial
 #ifdef serial
   Serial.print((outputValue0*ofvmult)/1000);
   Serial.print(".");
   Serial.print((outputValue0*ofvmult)%1000);
   Serial.print("ms reax= ");
   Serial.print(outputValue1);
-  Serial.println(" ms ");
+  Serial.print(" ms ");
+  Serial.println (outputValue0*ofvmult);
 #endif
 digitalWrite(IngectorPin, HIGH);
 delayMicroseconds(outputValue0*ofvmult);
