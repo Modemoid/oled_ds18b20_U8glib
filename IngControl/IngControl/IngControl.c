1556e8c226dd08a1db9d3072f1dd1a4445243e9d
Modemoid <modemoid@gmail.com>
2015-10-27 16:26:17 +0600

    minor coding

--- IngControl/IngControl/IngControl.c
+++ IngControl/IngControl/IngControl.c
@@ -10,12 +10,31 @@
 #define Ing0DDR DDRB
 #define Ing0PORT PORTB
 #define Ing0PIN PINB
-#define Ing0PinNum 3
+#define Ing0PinNum 3 //Ingector attcahed
 
 #define Ing1DDR DDRB
 #define Ing1PORT PORTB
 #define Ing1PIN PINB
 #define Ing1PinNum 5 //Arduino led attached
+
+
+#define PotDrocelDDR DDRC 
+#define PotDrocelPORT PORTC
+#define PotDrocelPIN PINC
+#define PotDrocelPinNum 0 //Drocel pot attached
+
+#define Pot0DDR DDRC
+#define Pot0PORT PORTC
+#define Pot0PIN PINC
+#define Pot0PinNum 1
+
+
+#define IgiDDR DDRD
+#define IgiPORT PORTD
+#define IgiPIN PIND
+#define IgiPinNum 2 //int0
+
+
 
 
 #include <avr/io.h>
@@ -25,15 +44,34 @@ int main(void)
 	
 	//DDRB = 0xff //all B is out
 	//DDRB = 0x00 //all B is in
-	
-	Ing0DDR  = _BV(Ing0PinNum);
-	Ing0DDR  = _BV(Ing1PinNum);
-	
 	//PORTD |= _BV(PD3);      // установить "1" на линии 3 порта D
 	//PORTD &= ~_BV(PD4);      // установить "0" на линии 4 порта D
+	
+	Ing0DDR  = _BV(Ing0PinNum);//ingector //out
+	Ing0DDR  = _BV(Ing1PinNum);// arduino led //out 
+	
+	IgiDDR = ~_BV(IgiPinNum); //igition sensor //in
+	PotDrocelDDR = ~_BV(PotDrocelPinNum); //Drocel pot attached //in
+	Pot0DDR = ~_BV(Pot0PinNumPinNum); //Drocel pot attached //in
+	
+
+StartSetup();
 	
     while(1)
     {
         //TODO:: Please write your application code 
     }
+}
+void ADCSetup(void)
+{
+	ADMUX = 0x00100000;    //(aref, only adch byte used, ch0 used) 
+	//ADMUX = 0x00100001;    //(aref, only adch byte used, ch1 used) 
+	
+	ADCSR = 0x10001110; //(ADC EN,
+						//silgle ADC convert NOT start,
+						//ADC free run OFF,
+						//ADC in flag(HW set)
+						//ADC int enabled,
+						//speed /64(250khz) adps2:0 110 )
+	  ADCSRA |=(1<<ADSC); //silgle ADC convert start,
 }
