117fac4911d5a81d213df15b0959c27f0bf78cb2
Pavel Kartsev <modemoid@gmail.com>
2015-12-21 08:30:48 +0600

    minor change

--- nokia/nokia.ino
+++ nokia/nokia.ino
@@ -28,6 +28,10 @@ U8GLIB_PCD8544 u8g(12, 11, 10, 9, 8);		// SPI Com: SCK = 13, MOSI = 11, CS = 10,
 #define TurnSig
 #define ArrOffset 1
 
+#define RTurnPin 2
+#define LTurnPin 4
+
+
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int sensorLat = 0;
@@ -76,58 +80,58 @@ const uint8_t accum_bitmap[] PROGMEM = {
 #endif
 #ifdef HeadLight
 const uint8_t hibeam_bitmap[] PROGMEM = {
-0b00001000,
-0b00111000,
-0b01101111,
-0b10001000,
-0b10001111,
-0b10001000,
-0b01101111,
-0b00111000,
-0b00001000
+  0b00001000,
+  0b00111000,
+  0b01101111,
+  0b10001000,
+  0b10001111,
+  0b10001000,
+  0b01101111,
+  0b00111000,
+  0b00001000
 };
 const uint8_t lobeam_bitmap[] PROGMEM = {
-0b00001000,
-0b00111000,
-0b01111100,
-0b11001010,
-0b10001001,
-0b11001100,
-0b01111010,
-0b00111001,
-0b00001000
+  0b00001000,
+  0b00111000,
+  0b01111100,
+  0b11001010,
+  0b10001001,
+  0b11001100,
+  0b01111010,
+  0b00111001,
+  0b00001000
 };
 #endif
 const uint8_t rArr_bitmap[] PROGMEM = {
-0b00001000,
-0b00001100,
-0b01111110,
-0b01111111,
-0b01111110,
-0b00001100,
-0b00001000
+  0b00001000,
+  0b00001100,
+  0b01111110,
+  0b01111111,
+  0b01111110,
+  0b00001100,
+  0b00001000
 };
 const uint8_t lArr_bitmap[] PROGMEM = {
-0b00010000,
-0b00110000,
-0b01111110,
-0b11111110,
-0b01111110,
-0b00110000,
-0b00010000
+  0b00010000,
+  0b00110000,
+  0b01111110,
+  0b11111110,
+  0b01111110,
+  0b00110000,
+  0b00010000
 };
 const uint8_t chkeng_bitmap[] PROGMEM = {
-0b00000111,0b11000000,
-0b00000001,0b00000000,
-0b00011111,0b11100000,
-0b00100000,0b00010000,
-0b00100001,0b00010111,
-0b10100001,0b00011101,
-0b11100001,0b00000001,
-0b10100000,0b00011101,
-0b00110001,0b00010111,
-0b00001000,0b00010000,
-0b00000111,0b11100000
+  0b00000111, 0b11000000,
+  0b00000001, 0b00000000,
+  0b00011111, 0b11100000,
+  0b00100000, 0b00010000,
+  0b00100001, 0b00010111,
+  0b10100001, 0b00011101,
+  0b11100001, 0b00000001,
+  0b10100000, 0b00011101,
+  0b00110001, 0b00010111,
+  0b00001000, 0b00010000,
+  0b00000111, 0b11100000
 };
 
 //end bitmap
@@ -209,37 +213,47 @@ void u8g_box_frame(uint8_t a) {
 #ifdef Accum_mark
   u8g.drawBitmapP(HBarW + 1 + acuOffsetW, heightLCD - 8 + acuOffsetH, 2, 8, accum_bitmap);
   sprintf (buf, "%d.%dV", VoltValue / 10, VoltValue % 10);
-  u8g.drawStr(HBarW + 18+ acuOffsetW, heightLCD - 8 + acuOffsetH, buf);
+  u8g.drawStr(HBarW + 18 + acuOffsetW, heightLCD - 8 + acuOffsetH, buf);
 
 #endif
 #ifdef HeadLight
 
-u8g.drawBitmapP( HBarW+2+10, 0, 1, 9, lobeam_bitmap);
+  u8g.drawBitmapP( HBarW + 2 + 10, 0, 1, 9, lobeam_bitmap);
 
-u8g.drawBitmapP( HBarW+2+9+10,0, 1, 9, hibeam_bitmap);
+  u8g.drawBitmapP( HBarW + 2 + 9 + 10, 0, 1, 9, hibeam_bitmap);
 
 #endif
 #ifdef CheckEng
-u8g.drawBitmapP( HBarW+2+36, heightLCD/2, 2, 11, chkeng_bitmap);
+  u8g.drawBitmapP( HBarW + 2 + 36, heightLCD / 2, 2, 11, chkeng_bitmap);
 #endif
 
 #ifdef TurnSig
 
-u8g.drawLine(HBarW+1,8+ArrOffset, HBarW+1+9,8+ArrOffset);
-u8g.drawLine(HBarW+1+9,ArrOffset, HBarW+1+9,8+ArrOffset);
+  u8g.drawLine(HBarW + 1, 8 + ArrOffset, HBarW + 1 + 9, 8 + ArrOffset);
+  u8g.drawLine(HBarW + 1 + 9, ArrOffset, HBarW + 1 + 9, 8 + ArrOffset);
 
-u8g.drawLine(widthLCD-HBarW -2 -10,8+ArrOffset, widthLCD-HBarW -2-10+9,8+ArrOffset);
-u8g.drawLine(widthLCD-HBarW -2 -10,8+ArrOffset, widthLCD-HBarW -2-10+9-9,8+ArrOffset-9);
+  u8g.drawLine(widthLCD - HBarW - 2 - 10, 8 + ArrOffset, widthLCD - HBarW - 2 - 10 + 9, 8 + ArrOffset);
+  u8g.drawLine(widthLCD - HBarW - 2 - 10, 8 + ArrOffset, widthLCD - HBarW - 2 - 10 + 9 - 9, 8 + ArrOffset - 9);
 
 
-  switch (TurnBlink) {
-    case 1: u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap); break;
-    case 2: u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap);; break;
-    case 3: {  u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap);
-  u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap);      
-      }; break;
-      
+  if (digitalRead(RTurnPin))
+  {
+    u8g.drawBitmapP( widthLCD - HBarW - 2 - 8, ArrOffset, 1, 7, rArr_bitmap);
   }
+  if (digitalRead(LTurnPin))
+  {
+    u8g.drawBitmapP( HBarW + 2, ArrOffset, 1, 7, lArr_bitmap);
+  }
+
+  /*  switch (TurnBlink) { //cycle show for test 
+      case 1: u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap); break;
+      case 2: u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap); break;
+      case 3: {  u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap);
+    u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap);
+        }; break;
+
+    }
+    */
 #endif
 
 
@@ -308,6 +322,10 @@ void setup(void) {
   pinMode(3, OUTPUT); //backlight
   analogWrite(3, 0);
 
+  pinMode(RTurnPin, INPUT);
+  pinMode(LTurnPin, INPUT);
+
+
 
 }
 
@@ -327,7 +345,9 @@ void loop(void) {
   // rebuild the picture after some delay
 
   delay(50);
-  
+
+  //   digitalWrite(13,digitalRead(2));
+
   if (latency < Latcount)
   {
     latency++;
@@ -359,8 +379,8 @@ void loop(void) {
   {
     LbarH = 0;
     TurnBlink++;
-    if (TurnBlink > 3 ){
-    TurnBlink=0;
+    if (TurnBlink > 3 ) {
+      TurnBlink = 0;
     }
   }
 }
