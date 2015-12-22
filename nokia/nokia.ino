cff6b72235be4cc7eff3b35fe36e8b22b71a856b
Modemoid <modemoid@gmail.com>
2015-12-22 13:58:37 +0600

    screen redraw.
    added big arrows partly kill draw state(may be will use again into
    future).

--- nokia/nokia.ino
+++ nokia/nokia.ino
@@ -13,7 +13,7 @@
 // setup u8g object, please remove comment from one of the following constructor calls
 // IMPORTANT NOTE: The following list is incomplete. The complete list of supported
 // devices with all constructor calls is here: https://github.com/olikraus/u8glib/wiki/device
-U8GLIB_PCD8544 u8g(12, 11, 10, 9, 8);		// SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 9, Reset = 8
+U8GLIB_PCD8544 u8g(12, 11, 10, 7, 8);		// SPI Com: SCK = 13, MOSI = 11, CS = 10, A0 = 7, Reset = 8
 
 
 #define Latcount 10
@@ -22,36 +22,66 @@ U8GLIB_PCD8544 u8g(12, 11, 10, 9, 8);		// SPI Com: SCK = 13, MOSI = 11, CS = 10,
 #define Fuel_Bar_mark
 #define HBarW 10
 #define Accum_mark
+#define AccHeight 10 //mark height +1
 #define acuOffsetW 0//по ширинеот левого бара 
-#define acuOffsetH -5 //по высоте от нижнего края
+#define acuOffsetH -0 //по высоте от нижнего края
+
+#define TopPicHeight 16
+
 #define HeadLight
 #define TurnSig
-#define ArrOffset 1
+//#define smallarrow
+#define ArrOffset 0
 
-#define RTurnPin 2
-#define LTurnPin 4
+#define backlight 9 //backlight pin. Must can PWM
 
+#define SigTable //draw table greed around top icons
 
-int sensorPin = A0;    // select the input pin for the potentiometer
-int sensorValue = 0;  // variable to store the value coming from the sensor
+#define Left_Bar
+#define Left_Bar_mark
+
+//manual defined screen size for nokia
+#define heightLCD 48 //if not defined be calculated
+#define widthLCD 84
+//end manual defined screen size
+
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+
+#define RTurnPin 2 //attach pins
+#define LTurnPin 3 //attach pins
+#define HeadBemPin 4 //attach pins
+#define LoBemPin 5 //attach pins
+
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+
+int sensorPin = A0;   //Fuel level pin
+int sensorValue = 0;  //
 int sensorLat = 0;
 
-int PowerPin = A1;
+int PowerPin = A1; //"Power" level pin
 int PowerValue = 0;
 int PowerLat = 0;
 
-int VoltPin = A2;
+int VoltPin = A2; //BoardPower net  pin
 int VoltValue = 0;
 int VoltLat = 0;
 
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
+//ALL EXTERNAL PINS MUST HAVE LEVEL LIMITERS (ZENNER diode)
 char latency = 0;
 int DrawH = 0;
 int FuelStart = 70, FuelEnd = 950; //ADC offset for fuel LVL sevsor
 char FuelLVLProcent = 0;
 char LbarH = 0;
 
+#ifdef TurnDebug
 char TurnBlink = 1;
-
+#endif
 //bitmap
 #ifdef Fuel_mark
 const uint8_t fuel_bitmap[] PROGMEM = {
@@ -102,6 +132,7 @@ const uint8_t lobeam_bitmap[] PROGMEM = {
   0b00001000
 };
 #endif
+#ifdef smallarrow
 const uint8_t rArr_bitmap[] PROGMEM = {
   0b00001000,
   0b00001100,
@@ -120,6 +151,7 @@ const uint8_t lArr_bitmap[] PROGMEM = {
   0b00110000,
   0b00010000
 };
+#endif
 const uint8_t chkeng_bitmap[] PROGMEM = {
   0b00000111, 0b11000000,
   0b00000001, 0b00000000,
@@ -134,30 +166,70 @@ const uint8_t chkeng_bitmap[] PROGMEM = {
   0b00000111, 0b11100000
 };
 
+const uint8_t BigRArr_bitmap[] PROGMEM = {
+  0b00001110, 0b00000000,
+  0b00000111, 0b00000000,
+  0b00000011, 0b10000000,
+  0b00000001, 0b11000000,
+  0b11111111, 0b11100000,
+  0b01111111, 0b11110000,
+  0b00111111, 0b11111000,
+  0b00011111, 0b11111100,
+  0b00111111, 0b11111000,
+  0b01111111, 0b11110000,
+  0b11111111, 0b11100000,
+  0b00000001, 0b11000000,
+  0b00000011, 0b10000000,
+  0b00000111, 0b00000000,
+  0b00001110, 0b00000000,
+};
+
+const uint8_t BigLArr_bitmap[] PROGMEM = {
+
+  0b00000001,  0b11000000,
+  0b00000011, 0b10000000,
+  0b00000111, 0b00000000,
+  0b00001110, 0b00000000,
+  0b00011111, 0b11111100,
+  0b00111111, 0b11111000,
+  0b01111111, 0b11110000,
+  0b11111111, 0b11100000,
+  0b01111111, 0b11110000,
+  0b00111111, 0b11111000,
+  0b00011111, 0b11111100,
+  0b00001110, 0b00000000,
+  0b00000111, 0b00000000,
+  0b00000011, 0b10000000,
+  0b00000001, 0b11000000
+
+};
+
 //end bitmap
 
 
 void u8g_prepare(void) {
-  // u8g.setFont(u8g_font_6x10);
+  //u8g.setFont(u8g_font_6x10);
   //u8g.setFont(u8g_font_04b_24n);
-  u8g.setFont(u8g_font_5x8);
+  u8g.setFont(u8g_font_5x8); //~2kb
   u8g.setFontRefHeightExtendedText();
   u8g.setDefaultForegroundColor();
   u8g.setFontPosTop();
 }
 
-void u8g_box_frame(uint8_t a) {
-  //  u8g.drawStr( 0, 0, "drawBox");
+void u8g_1screen(uint8_t a) {
+
+
   char buf[8];
+#ifndef heightLCD
   char heightLCD, widthLCD;
 
   heightLCD = u8g.getHeight(); //высота
   widthLCD = u8g.getWidth(); //ширина
-
+#endif
 #ifdef ADC_debug
   //u8g.drawFrame(0,0,84,48);nokia 5110 lcd frame
   sprintf (buf, "a0=%d", sensorValue);
-  u8g.drawStr(widthLCD / 2 - 10, 0, buf);
+  u8g.drawStr(widthLCD / 2 - 10, 17, buf);
 #endif
   // fuel LVL
   // xx% near bar
@@ -168,7 +240,7 @@ void u8g_box_frame(uint8_t a) {
   u8g.drawFrame(widthLCD - HBarW , 0, HBarW, heightLCD - DrawH);
   u8g.drawBox(widthLCD - HBarW , heightLCD - DrawH, HBarW, heightLCD);
 #ifdef Fuel_Bar_mark
-  //bar mark
+  //fuel bar mark
   u8g.drawPixel(widthLCD - HBarW - 1, heightLCD / 2);
   u8g.drawPixel(widthLCD - HBarW - 2, heightLCD / 2);
 
@@ -200,7 +272,7 @@ void u8g_box_frame(uint8_t a) {
   u8g.drawPixel(widthLCD - HBarW + 4, (heightLCD / 4) * 3);
 
   u8g.setColorIndex(1);
-  //end bar mark
+  //end fuel bar mark
 #endif
   //end bar
 #ifdef Fuel_mark
@@ -215,85 +287,111 @@ void u8g_box_frame(uint8_t a) {
   sprintf (buf, "%d.%dV", VoltValue / 10, VoltValue % 10);
   u8g.drawStr(HBarW + 18 + acuOffsetW, heightLCD - 8 + acuOffsetH, buf);
 
+  u8g.drawLine(HBarW + 1, heightLCD - AccHeight, widthLCD - (HBarW + 2), heightLCD - AccHeight);
+  u8g.drawLine(55, heightLCD, 55, heightLCD - AccHeight);
+
+
 #endif
 #ifdef HeadLight
 
-  u8g.drawBitmapP( HBarW + 2 + 10, 0, 1, 9, lobeam_bitmap);
+  u8g.drawBitmapP(28, 0, 1, 9, lobeam_bitmap);
 
-  u8g.drawBitmapP( HBarW + 2 + 9 + 10, 0, 1, 9, hibeam_bitmap);
+  u8g.drawBitmapP(44, 0, 1, 9, hibeam_bitmap);
 
 #endif
+
 #ifdef CheckEng
   u8g.drawBitmapP( HBarW + 2 + 36, heightLCD / 2, 2, 11, chkeng_bitmap);
 #endif
 
-#ifdef TurnSig
 
-  u8g.drawLine(HBarW + 1, 8 + ArrOffset, HBarW + 1 + 9, 8 + ArrOffset);
-  u8g.drawLine(HBarW + 1 + 9, ArrOffset, HBarW + 1 + 9, 8 + ArrOffset);
-
-  u8g.drawLine(widthLCD - HBarW - 2 - 10, 8 + ArrOffset, widthLCD - HBarW - 2 - 10 + 9, 8 + ArrOffset);
-  u8g.drawLine(widthLCD - HBarW - 2 - 10, 8 + ArrOffset, widthLCD - HBarW - 2 - 10 + 9 - 9, 8 + ArrOffset - 9);
-
-
-  if (digitalRead(RTurnPin))
-  {
-    u8g.drawBitmapP( widthLCD - HBarW - 2 - 8, ArrOffset, 1, 7, rArr_bitmap);
-  }
-  if (digitalRead(LTurnPin))
-  {
-    u8g.drawBitmapP( HBarW + 2, ArrOffset, 1, 7, lArr_bitmap);
-  }
-
-  /*  switch (TurnBlink) { //cycle show for test 
-      case 1: u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap); break;
-      case 2: u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap); break;
-      case 3: {  u8g.drawBitmapP( HBarW+2, ArrOffset, 1, 7, lArr_bitmap);
-    u8g.drawBitmapP( widthLCD-HBarW -2 -8, ArrOffset, 1, 7, rArr_bitmap);
-        }; break;
-
-    }
-    */
+#ifdef SigTable
+  //signal table greed
+  u8g.drawLine(HBarW + 1, TopPicHeight, widthLCD - (HBarW + 2), TopPicHeight);
+  u8g.drawLine(26, 0, 26, TopPicHeight);
+  u8g.drawLine(41, 0, 41, TopPicHeight);
+  u8g.drawLine(42, 0, 42, TopPicHeight);
+  u8g.drawLine(57, 0, 57, TopPicHeight);
+  //end signal table greed
 #endif
 
 
+#ifdef TurnSig
+
+  if (digitalRead(LTurnPin))
+  {
+    u8g.drawBitmapP( HBarW + 1 , ArrOffset, 2, 15, BigLArr_bitmap); //big
+    //u8g.drawBitmapP( HBarW + 2, ArrOffset, 1, 7, lArr_bitmap); //small
+  }
+
+  if (digitalRead(RTurnPin))
+  {
+    u8g.drawBitmapP( widthLCD - HBarW - 1 - 14 , ArrOffset, 2, 15, BigRArr_bitmap);//big
+    //u8g.drawBitmapP( widthLCD - HBarW - 2 - 8, ArrOffset, 1, 7, rArr_bitmap);//small
+  }
+
+#ifdef TurnDebug //hardcodec small arrow only
+  switch (TurnBlink) { //cycle show for test
+    case 1: u8g.drawBitmapP( HBarW + 2, ArrOffset, 1, 7, lArr_bitmap); break;
+    case 2: u8g.drawBitmapP( widthLCD - HBarW - 2 - 8, ArrOffset, 1, 7, rArr_bitmap); break;
+    case 3: {
+        u8g.drawBitmapP( HBarW + 2, ArrOffset, 1, 7, lArr_bitmap);
+        u8g.drawBitmapP( widthLCD - HBarW - 2 - 8, ArrOffset, 1, 7, rArr_bitmap);
+      }; break;
+  }
+#endif
+#endif
+
+#ifdef Left_Bar
   //left bar
   u8g.drawFrame(0 , 0, HBarW, heightLCD - LbarH );
   u8g.drawBox(0, heightLCD - LbarH, HBarW, heightLCD);
-  //end of Left bar
 
+#ifdef Left_Bar_mark
+  //Left bar mark
+  u8g.drawPixel(HBarW + 1, heightLCD / 2);
+  
+  u8g.drawPixel(HBarW - 1 - 5, heightLCD / 2);
+  u8g.drawPixel(HBarW + 1 - 5, heightLCD / 2);
+  u8g.drawPixel(HBarW + 3 - 5, heightLCD / 2);
+  u8g.drawPixel(HBarW + 5 - 5, heightLCD / 2);
+
+  u8g.drawPixel(HBarW - 1 - 5, heightLCD / 4);
+  u8g.drawPixel(HBarW + 1 - 5, heightLCD / 4);
+  u8g.drawPixel(HBarW + 3 - 5, heightLCD / 4);
+  u8g.drawPixel(HBarW + 5 - 5, heightLCD / 4);
+
+  u8g.drawPixel(HBarW - 1 - 5, (heightLCD / 4) * 3);
+  u8g.drawPixel(HBarW + 1 - 5, (heightLCD / 4) * 3);
+  u8g.drawPixel(HBarW + 3 - 5, (heightLCD / 4) * 3);
+  u8g.drawPixel(HBarW + 5 - 5, (heightLCD / 4) * 3);
+
+  u8g.setColorIndex(0);
+  u8g.drawPixel(HBarW - 5, heightLCD / 2);
+  u8g.drawPixel(HBarW + 2 - 5, heightLCD / 2);
+  u8g.drawPixel(HBarW + 4 - 5, heightLCD / 2);
+
+  u8g.drawPixel(HBarW - 5, heightLCD / 4);
+  u8g.drawPixel(HBarW + 2 - 5, heightLCD / 4);
+  u8g.drawPixel(HBarW + 4 - 5, heightLCD / 4);
+
+  u8g.drawPixel(HBarW - 5, (heightLCD / 4) * 3);
+  u8g.drawPixel(HBarW + 2 - 5, (heightLCD / 4) * 3);
+  u8g.drawPixel(HBarW + 4 - 5, (heightLCD / 4) * 3);
+
+  u8g.setColorIndex(1);
+  //end Left bar mark
+#endif
+  //end of Left bar
+#endif
 
 
 #ifdef ADC_debug
   //Raw a1 out
   sprintf (buf, "a1=%d", PowerValue);
-  u8g.drawStr(widthLCD / 2 - 10, 10, buf);
+  u8g.drawStr(widthLCD / 2 - 10, 25, buf);
 #endif
   //sensorValue
-}
-
-void u8g_ascii_1() {
-  char s[2] = " ";
-  uint8_t x, y;
-  u8g.drawStr( 0, 0, "ASCII page 1");
-  for ( y = 0; y < 6; y++ ) {
-    for ( x = 0; x < 16; x++ ) {
-      s[0] = y * 16 + x + 32;
-      u8g.drawStr(x * 7, y * 10 + 10, s);
-    }
-  }
-}
-
-void u8g_ascii_2() {
-  char s[2] = " ";
-  uint8_t x, y;
-  u8g.drawStr( 0, 0, "ASCII page 2");
-  for ( y = 0; y < 6; y++ ) {
-    for ( x = 0; x < 16; x++ ) {
-      s[0] = y * 16 + x + 160;
-      u8g.drawStr(x * 7, y * 10 + 10, s);
-    }
-  }
 }
 
 
@@ -302,12 +400,7 @@ uint8_t draw_state = 0;
 
 void draw(void) {
   u8g_prepare();
-  switch (draw_state >> 3) {
-    case 0: u8g_box_frame(draw_state & 7); break;
-      //case 6: u8g_ascii_1(); break;
-      //case 7: u8g_ascii_2(); break;
-
-  }
+  u8g_1screen(draw_state);
 }
 
 
@@ -319,19 +412,17 @@ void setup(void) {
   pinMode(13, OUTPUT); //arduino led pin
   digitalWrite(13, HIGH);
 
-  pinMode(3, OUTPUT); //backlight
-  analogWrite(3, 0);
+  pinMode(backlight, OUTPUT); //backlight
+  analogWrite(backlight, 1);
 
   pinMode(RTurnPin, INPUT);
   pinMode(LTurnPin, INPUT);
-
-
 
 }
 
 void loop(void) {
 
-  // picture loop
+
   u8g.firstPage();
   do {
     draw();
@@ -378,10 +469,12 @@ void loop(void) {
   } else
   {
     LbarH = 0;
+#ifdef TurnDebug
     TurnBlink++;
     if (TurnBlink > 3 ) {
       TurnBlink = 0;
     }
+#endif
   }
 }
 
